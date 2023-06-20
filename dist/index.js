96fa72ed474a559d87177a08bda06c78a0f1e8be
JHLee <vps32@naver.com>
2023-06-20 13:06:17 +0900

    feat: prompt update

--- dist/index.js
+++ dist/index.js
@@ -37978,24 +37978,41 @@ module.exports = {
 
 function prompt(){
 
+    let prompt = ""
+    prompt += roles()
+    prompt += actions()
+    prompt += answer()
+    return prompt
+}
+
+function roles(){
+    let prompt = "I want you to act as a Senior Developer reviewer." //시니어 개발자 리뷰어로 활동해 주셨으면 합니다.
+    return prompt
+}
+
+function actions(){
+
+    let prompt = "View your colleagues' git commits and provide code review and feedback" //동료의 Git 커밋을 보고 코드 검토 및 피드백을 제공하세요.
+    prompt += "Check the code quality" //코드 품질 확인
+    prompt += "Analyzing the possibility of bugs or issues" //버그 또는 문제의 가능성 분석하기
+    prompt += "Inspecting the readability" //가독성 검사하기
+    prompt += "Evaluating if needs optimization" //최적화가 필요한지 평가
+    prompt += "Verification based on SOLED principles" //SOLED 원칙에 기반하여 확인
+
+    prompt += "If that's not exactly what's going on don't say anything, just say \"LGTM\". \n" //그런 일이 아니라면 아무 말도 하지 말고 그냥 "LGTM"이라고 말하세요.
+
+    return prompt
+}
+function answer(){
 
     let prompt = ""
-    prompt += "I want you to act as a Senior Developer reviewer." //시니어 개발자 리뷰어로 활동해 주셨으면 합니다.
+    prompt += "Keep your answers short." //답변은 짧게
     prompt += "Your responses should be informative and logical." //답변은 유익하고 논리적으로 작성해야 합니다.
     prompt += "You should always adhere to technical information." //항상 기술 정보를 준수해야 합니다.
-    prompt += "Keep your answers short." //답변은 짧게
     prompt += "Use Markdown formatting in your answers." //답변에 마크다운 서식을 사용.
     prompt += "Make sure to include the programming language name at the start of the Markdown code blocks." //마크다운 코드 블록의 시작 부분에 프로그래밍 언어 이름을 포함해야 합니다.
-
-    prompt += "Note to code reviewers" //코드 검토자를 위한 참고 사항
-    prompt += "1. Check the code quality" //최근 커밋의 코드 품질 확인
-    prompt += "2. Analyzing the possibility of bugs or issues" //최근 변경 사항의 버그 또는 문제 가능성 분석
-    prompt += "3. Inspecting the readability" //커밋된 코드의 가독성 검사하기
-    prompt += "4. Evaluating if the code in the commit needs optimization" //커밋의 코드에 최적화가 필요한지 평가하기
-
-    prompt += "View your colleagues' git commits and provide code review and feedback" //동료의 Git 커밋을 보고 코드 검토 및 피드백을 제공하세요.
-    prompt += "If that's not exactly what's going on don't say anything, just say \"LGTM\". \n" //그런 일이 아니라면 아무 말도 하지 말고 그냥 "LGTM"이라고 말하세요.
     prompt += "Please translate your answer into Korean" //답변은 한국어로 번역해주세요
+
     return prompt
 }
 module.exports = {
